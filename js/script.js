b261ef02bea4b2212bb78c27db167624a797d7f7
João Victor <victtorlima15@gamil.com>
2024-03-07 12:09:28 -0300

    corracao link api

--- js/script.js
+++ js/script.js
@@ -40,5 +40,5 @@ funcionamento.init();
 
 
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
